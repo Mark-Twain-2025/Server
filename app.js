2224e6b4c2e21ed1df71833276964efb61e8423d
minju00 <bin3994000@naver.com>
2025-06-25 09:40:31 +0900

    feat: #1 db 연결

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
-//hi
+// db 관리를 위한 dotenv 설치 및 불러오기
+const dotenv = require("dotenv");
+dotenv.config();
 
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+// Router 불러오기
+const voteRouter = require("./routes/vote");
+
+const mongoose = require("mongoose");
+const MONGO_HOST = process.env.DB_URL;
+
+mongoose
+  .connect(MONGO_HOST, {
+    retryWrites: true,
+    w: "majority",
+  })
+  .then(() => {
+    console.log("Connected Successful");
+  })
+  .catch((err) => {
+    console.log("error");
+  });
 
 var app = express();
 
@@ -15,14 +34,15 @@ var app = express();
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/vote", voteRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -33,11 +53,11 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  // res.render("error");
 });
 
 module.exports = app;
