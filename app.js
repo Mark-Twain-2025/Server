7f390d6e8646dcba47baa2e1e69b4c737a958754
minju00 <bin3994000@naver.com>
2025-06-27 10:07:23 +0900

    feat:#4 quiz

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ const signupRouter = require("./routes/signup");
 const investRouter = require("./routes/investments");
 const quizRouter = require("./routes/quiz");
 const quizHistoryRouter = require("./routes/quiz_history");
+const userInfoRouter = require("./routes/user_info");
 
 const mongoose = require("mongoose");
 const MONGO_HOST = process.env.DB_URL;
@@ -78,6 +79,7 @@ app.use("/investments", investRouter);
 
 app.use("/quiz", quizRouter);
 app.use("/quizHistory", quizHistoryRouter);
+app.use("/user_info", userInfoRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
