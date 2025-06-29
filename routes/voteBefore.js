275850299439a9f85325cfb1348179efc7f03c1b
minju00 <bin3994000@naver.com>
2025-06-29 21:17:17 +0900

    feat: #42 투표 후 user_info coins 차감

--- routes/voteBefore.js
+++ routes/voteBefore.js
@@ -2,13 +2,21 @@ const express = require("express");
 const router = express.Router();
 const VoteBefore = require("../models/VoteBefore");
 const VoteAfter = require("../models/VoteAfter");
-
+const UserInfo = require("../models/UserInfo");
 // 테스트용 더미 데이터 생성 API 삭제
 
 router.post("/:userId", async (req, res, next) => {
   try {
     const { category_id, amount, date } = req.body;
     const { userId } = req.params;
+
+    const userInfo = await UserInfo.findOne({ user_id: parseInt(userId) });
+    if (!userInfo) {
+      return res.status(404).json({ error: "유저 정보를 찾을 수 없습니다." });
+    }
+    if (userInfo.coins < amount) {
+      return res.status(400).json({ error: "보유 코인이 부족합니다." });
+    }
     console.log(req.body);
     const votebefore = await VoteBefore.create({
       user_id: userId,
@@ -16,7 +24,15 @@ router.post("/:userId", async (req, res, next) => {
       category_id: category_id,
       amount: amount,
     });
-    res.status(201).json(votebefore);
+    // user_info의 coins 수 차감
+    userInfo.coins -= amount;
+    await userInfo.save();
+
+    console.log("성공!!");
+    res.status(201).json({
+      vote: votebefore,
+      remainingCoins: userInfo.coins,
+    });
   } catch (err) {
     console.error(err);
     res.status(400);
